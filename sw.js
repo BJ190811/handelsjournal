579e56cffa332b47f387e2f2c5eb581b8c85eb42
BJ190811 <bjhighclass89@gmail.com>
2026-07-16 04:29:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'handelsjournal-v2';
+const CACHE_NAME = 'handelsjournal-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './bg-texture.jpg'];
 
 self.addEventListener('install', (event) => {
